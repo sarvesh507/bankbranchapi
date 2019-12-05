0b11f7decf27a215e10a48bf32fbb4ffe23dff83
sarvesh507 <sarvesh507@github.com>
2019-12-06 00:49:05 +0530

    ok

--- bankbranchapi/settings.py
+++ bankbranchapi/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'bankbranchapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
